6084ceac152c528b4e913a5d07ff2b1a62677e2b
Oleg <savochkinoleg17@gmail.com>
2023-04-24 17:55:49 +0300

    build:create architecture

--- base/settings.py
+++ base/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #external apps
+    # external apps
     'django_extensions',
     # own apps,
     'users',
